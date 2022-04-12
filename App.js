69b631e829f5ff472c4434b2537ed632d78dd884
Sachin Singh <sachinsinghiitkgp@gmail.com>
2022-04-13 01:05:35 +0530

    basic express app

--- App.js
+++ App.js
@@ -1,2 +1,9 @@
 const express  = require('express');
 const App = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 8000;
+
+
+App.listen(PORT,()=>{
+    console.log("Welcome, sorry for inconvinience will be back in  weeks");
+})
