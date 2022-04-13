7081d990cee30f3fd0c195438cc8061f33dff3b3
Sachin Singh <sachinsinghiitkgp@gmail.com>
2022-04-13 15:34:41 +0530

    first deploy

--- App.js
+++ App.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 8000
 
 
 app.listen(PORT,()=>{
-    console.log("Welcome, sorry for inconvinience will be back in  weeks");
+    console.log("Welcome, sorry for inconvinience will be back in  weeks "+ PORT);
 })
